211b551db3881850d2c50c3059e1ba8f12f38af0
abboskhan <abbosxon28012004@gmail.com>
2023-01-28 10:38:24 +0500

    all

--- src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
@@ -1,8 +1,21 @@
 package school.mjc.stage0.loops.finalTask;
 
+import java.util.Collections;
+
 public class HalfPyramid {
     public void printHalfPyramid(int cathetusLength) {
+        if (cathetusLength == 0) {
+            return;
+        }
+        String[] pyramid = new String[cathetusLength];
 
+        for (int i = 0; i < cathetusLength; i++) {
+            pyramid[i] = String.join("", Collections.nCopies(cathetusLength-i-1, " "))
+                    +String.join("", Collections.nCopies(i+1, "*"));
+        }
+        for (String s : pyramid) {
+            System.out.println(s);
+        }
     }
 
     public static void main(String[] args) {
--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -1,8 +1,23 @@
 package school.mjc.stage0.loops.finalTask;
 
+import java.util.Collections;
+
 public class Pyramid {
     public void printPyramid(int cathetusLength) {
+        if (cathetusLength == 0) {
+            return;
+        }
+        String str ="1" ;
+        String[] pyramid = new String[cathetusLength];
+        pyramid[0] = String.join("", Collections.nCopies(cathetusLength-1, " "))+ str;
 
+        for (int i = 2; i <= cathetusLength; i++) {
+            str = i + str + i;
+            pyramid[i - 1] = String.join("", Collections.nCopies(cathetusLength-i, " "))+str;
+        }
+        for (String s : pyramid) {
+            System.out.println(s);
+        }
     }
 
     public static void main(String[] args) {
